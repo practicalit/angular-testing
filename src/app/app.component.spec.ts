d9a981a4fa17f3d30e6cf37329b7265adf71a935
Practical IT <code@thepracticalit.com>
2021-08-11 08:31:23 -0400

    Initial angular testing project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed, async } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -6,6 +7,8 @@ describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        ReactiveFormsModule,
+        FormsModule,
         RouterTestingModule
       ],
       declarations: [
@@ -31,5 +34,16 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('angular-unit-testing app is running!');
+  });
+
+  it('should render first name on checkbox selection', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    let component = fixture.componentInstance;
+    let checkbox = component.contactForm.controls['showInput'];
+    component.handleChange();
+    checkbox.setValue(true);
+    fixture.detectChanges();//check changes after value change.
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.data').textContent).toContain('Add Data');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-unit-testing';
+  show: boolean = false;
+
+  constructor(private fb: FormBuilder) {
+
+  }
+  ngOnInit() {
+
+  }
+
+  contactForm = this.fb.group({
+    anyData: new FormControl(),
+    showInput: new FormControl(),
+  })
+
+  handleChange() {
+    this.show = !this.show;
+  }
 }
